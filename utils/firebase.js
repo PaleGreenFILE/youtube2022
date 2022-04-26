9a3fc1bed980723263afe8ab9fcffa5a11ecdb86
PaleGreenFile <charlyangels29@gmail.com>
2022-04-26 20:06:44 +0200

    commit

--- utils/firebase.js
+++ utils/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey:  process.env.API_KEY,
-  authDomain:  process.env.AUTH_DAOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEAS_I,
+  apiKey:  process.env.REACT_APP_API_KEY,
+  authDomain:  process.env.REACT_APP_AUTH_DAOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEAS_I,
 };
 
 // Initialize Firebase
