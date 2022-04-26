7b33ce06e8dd739389c8d469a2c14a261ba763b2
PaleGreenFile <charlyangels29@gmail.com>
2022-04-26 21:49:10 +0200

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 
 import Layout from "./../components/Layout";
+import AxeptioInjector from './../util/AxeptioInjector';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <AxeptioInjector/>
       <Component {...pageProps} />
     </Layout>
   );
